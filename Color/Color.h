5b36ac080784925e0fe635180feda2b496df1c38
christian otkjær <christian.otkjaer@gmail.com>
2016-10-27 14:47:48 +0200

    Initial Commit

---
+++ Color/Color.h
@@ -0,0 +1,19 @@
+//
+//  Color.h
+//  Color
+//
+//  Created by Christian Otkjær on 27/10/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Color.
+FOUNDATION_EXPORT double ColorVersionNumber;
+
+//! Project version string for Color.
+FOUNDATION_EXPORT const unsigned char ColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Color/PublicHeader.h>
+
+
